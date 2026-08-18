98e9ec77da65ae7ee7d24ed906eed8f3eeadeb02
JJH728 <72532462+JJH728@users.noreply.github.com>
2026-08-18 12:59:20 +0900

    Update

--- Assets/Detection/SoundEmitter.cs
+++ Assets/Detection/SoundEmitter.cs
@@ -9,18 +9,18 @@ namespace Squad
         private static readonly Collider[] _hits = new Collider[MaxHits];
         
         // pos에서 sound를 방출한다. sound.Radius 안에 있으면서 enemyLayer 안에 속한 개체에게 이 소리를 보고한다.
-        public static void Emit(Vector3 pos, Sound sound, LayerMask enemyLayer)
+        public static void Emit(Vector3 position, Sound sound, LayerMask enemyLayer)
         {
             if (SquadBlackboard.Instance == null)
                 return;
             
             int count = Physics.OverlapSphereNonAlloc(
-                pos, sound.Radius, _hits, enemyLayer, QueryTriggerInteraction.Ignore);
+                position, sound.Radius, _hits, enemyLayer, QueryTriggerInteraction.Ignore);
 
             if (count == 0)
                 return;
 
-            SquadBlackboard.Instance.ReportSound(pos, sound);
+            SquadBlackboard.Instance.ReportSound(position, sound);
         }
     }
 }
